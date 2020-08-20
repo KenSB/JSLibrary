3c7ba9a505172462de01a2fdb5cba9ffac6589ed
KenSB <kennethsb.wong@gmail.com>
2020-08-19 21:05:51 -0400

    Fixed the landing page

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 
 const express = require('express')
-
+const path = require('path')
 const app = express();
-app.use(express.static(__dirname + '/csc309selfproject'))
+
+// app.use(express.static(__dirname + '/csc309selfproject'))
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname+ '/csc309selfproject', '/landing.html'))
+// })
+
+// static js directory
+app.use("/", express.static(path.join(__dirname, '/csc309selfproject')))
+
+// route for root
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/csc309selfproject/landing.html'))
+})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
